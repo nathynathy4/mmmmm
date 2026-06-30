2fc0563be33547403860659874de8a90084a635c
nathynathy4 <sales.ribeiro@escola.pr.gov.br>
2026-06-30 09:27:49 -0300

    ntnt

--- main.js
+++ main.js
@@ -42,3 +42,25 @@ function calculaTempo(tempoObjetivo) {
   " segundos"
 );
 }
+function comecaCronometro(){
+      setInterval(atualizaCronometro(),1000);
+}
+//comecaCronometro();
+<div class="contador">
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">dias</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">horas</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">min</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">seg</p>
+  </div>
+</div>
